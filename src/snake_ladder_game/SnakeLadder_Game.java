e2c4661636c731a2d01d21b7f3edfe2692aea408
mudable <mudableshilpa@gmail.com>
2021-10-21 17:09:32 +0530

    Added:UC6 to count how many time the player roll the die

--- src/snake_ladder_game/SnakeLadder_Game.java
+++ src/snake_ladder_game/SnakeLadder_Game.java
@@ -2,6 +2,7 @@ package snake_ladder_game;
 
 public class SnakeLadder_Game {
 	static int position = 0;
+	static int count = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake Ladder game.");
@@ -9,7 +10,7 @@ public class SnakeLadder_Game {
 			int die = (int) (Math.random() * 10) % 7;
 			System.out.println("Position of player is:" + die);
 			int result = (int) (Math.random() * 10) % 3 + 1;
-
+			count++;
 			System.out.println(" switch case number is " + result);
 			switch (result) {
 			case 1:
@@ -30,14 +31,15 @@ public class SnakeLadder_Game {
 				System.out.println("Your Position after snake is::" + position);
 				break;
 			}
-			if(position>100) {
-				position=position=die;
+			if (position > 100) {
+				position = position = die;
 				System.out.println("your position after ladder is ::" + position);
 			}
-			if(position==100) {
+			if (position == 100) {
 				System.out.println("player won the game");
 			}
 		}
+		System.out.println("Number of time the player roll the die is:" + count);
 	}
 
 }
