bec337eeabc794eb355be3a88a4ce4591a91f70b
mudable <mudableshilpa@gmail.com>
2021-10-21 16:36:52 +0530

    Added:UC3 to set the no play,ladded and snake option using switch case

--- src/snake_ladder_game/SnakeLadder_Game.java
+++ src/snake_ladder_game/SnakeLadder_Game.java
@@ -5,8 +5,30 @@ public class SnakeLadder_Game {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake Ladder game.");
-		int position = (int) (Math.random() * 10) % 7;
-		System.out.println("Position of player1 is:" + position);
+		int die = (int) (Math.random() * 10) % 7;
+		System.out.println("Position of player is:" + die);
+		int result = (int) (Math.random() * 10) % 3 + 1;
+
+		System.out.println(" switch case number is " + result);
+		switch (result) {
+		case 1:
+			System.out.println("No Play You are in the same position.");
+			break;
+
+		case 2:
+			position = (position + die);
+			System.out.println("your position after ladder is ::" + position);
+			break;
+
+		case 3:
+			position = (position - die);
+
+			if (position < 0) {
+				position = 0;
+			}
+			System.out.println("Your Position after snake is::" + position);
+			break;
+		}
 	}
 
 }
