628b97c67612a3357904a088fb3a7293f865e387
mudable <mudableshilpa@gmail.com>
2021-10-21 16:50:52 +0530

    Added:UC4 to get the winning position 100

--- src/snake_ladder_game/SnakeLadder_Game.java
+++ src/snake_ladder_game/SnakeLadder_Game.java
@@ -5,29 +5,31 @@ public class SnakeLadder_Game {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake Ladder game.");
-		int die = (int) (Math.random() * 10) % 7;
-		System.out.println("Position of player is:" + die);
-		int result = (int) (Math.random() * 10) % 3 + 1;
+		while (position < 100) {
+			int die = (int) (Math.random() * 10) % 7;
+			System.out.println("Position of player is:" + die);
+			int result = (int) (Math.random() * 10) % 3 + 1;
 
-		System.out.println(" switch case number is " + result);
-		switch (result) {
-		case 1:
-			System.out.println("No Play You are in the same position.");
-			break;
+			System.out.println(" switch case number is " + result);
+			switch (result) {
+			case 1:
+				System.out.println("No Play You are in the same position.");
+				break;
 
-		case 2:
-			position = (position + die);
-			System.out.println("your position after ladder is ::" + position);
-			break;
+			case 2:
+				position = (position + die);
+				System.out.println("your position after ladder is ::" + position);
+				break;
 
-		case 3:
-			position = (position - die);
+			case 3:
+				position = (position - die);
 
-			if (position < 0) {
-				position = 0;
+				if (position < 0) {
+					position = 0;
+				}
+				System.out.println("Your Position after snake is::" + position);
+				break;
 			}
-			System.out.println("Your Position after snake is::" + position);
-			break;
 		}
 	}
 
