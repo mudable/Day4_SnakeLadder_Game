3d52c94ef035c09c93fb48dfdbc7a8b9298d4fc5
mudable <mudableshilpa@gmail.com>
2021-10-21 16:25:24 +0530

    Added:UC2 to roll the die to get number from 0 to 6

--- src/snake_ladder_game/SnakeLadder_Game.java
+++ src/snake_ladder_game/SnakeLadder_Game.java
@@ -5,6 +5,8 @@ public class SnakeLadder_Game {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake Ladder game.");
+		int position = (int) (Math.random() * 10) % 7;
+		System.out.println("Position of player1 is:" + position);
 	}
 
 }
