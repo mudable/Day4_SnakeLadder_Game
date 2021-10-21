73d886189b9d30e06b8ff88f058be10c75120862
mudable <mudableshilpa@gmail.com>
2021-10-21 15:51:06 +0530

    Added:UC1 to set the position as 0

--- src/snake_ladder_game/SnakeLadder_Game.java
+++ src/snake_ladder_game/SnakeLadder_Game.java
@@ -1,6 +1,8 @@
 package snake_ladder_game;
 
 public class SnakeLadder_Game {
+	static int position = 0;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake Ladder game.");
 	}
