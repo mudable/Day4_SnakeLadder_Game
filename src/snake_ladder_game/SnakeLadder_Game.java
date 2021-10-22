b6e94b5b445579518d59969c0a08f8fc49726c0b
mudable <mudableshilpa@gmail.com>
2021-10-22 17:08:10 +0530

    Added:UC7 for player2

--- src/snake_ladder_game/SnakeLadder_Game.java
+++ src/snake_ladder_game/SnakeLadder_Game.java
@@ -1,45 +1,71 @@
 package snake_ladder_game;
 
 public class SnakeLadder_Game {
-	static int position = 0;
+	int position = 0;
 	static int count = 0;
+	int position1 = 0;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to snake Ladder game.");
-		while (position < 100) {
-			int die = (int) (Math.random() * 10) % 7;
-			System.out.println("Position of player is:" + die);
-			int result = (int) (Math.random() * 10) % 3 + 1;
-			count++;
-			System.out.println(" switch case number is " + result);
-			switch (result) {
-			case 1:
-				System.out.println("No Play You are in the same position.");
-				break;
+	int position2 = 0;
 
-			case 2:
-				position = (position + die);
-				System.out.println("your position after ladder is ::" + position);
-				break;
+	public int start(int position) {
 
-			case 3:
-				position = (position - die);
+		this.position = position;
+		count = 0;
+		// position = 0;
 
-				if (position < 0) {
-					position = 0;
-				}
-				System.out.println("Your Position after snake is::" + position);
-				break;
+		int rollDice = position();
+		int checkValue = die();
+		switch (checkValue) {
+		case 0:
+			System.out.println("player in the same position");
+			break;
+		case 1:
+			position = (position + rollDice);
+			System.out.println("player position after ladder is :" + position);
+			break;
+		case 2:
+			position = (position - rollDice);
+			System.out.println("player Position after snake is:" + position);
+			if (position < 0) {
+				position = 0;
 			}
-			if (position > 100) {
-				position = position = die;
-				System.out.println("your position after ladder is ::" + position);
-			}
-			if (position == 100) {
-				System.out.println("player won the game");
-			}
+			break;
 		}
-		System.out.println("Number of time the player roll the die is:" + count);
+		if (position > 100) {
+			position = position - rollDice;
+			System.out.println("your position after ladder is :" + position);
+		}
+		return position;
+
 	}
 
+	public void checkWinner(SnakeLadder_Game obj) {
+		while (position1 < 100 && position2 < 100) {
+			position1 = obj.start(position1);
+			System.out.println("Position of player 1 is " + position1);
+			if (position1 == 100) {
+				System.out.println("player1 own the game");
+			}
+			position2 = obj.start(position2);
+			System.out.println("Position of player 2 is " + position2);
+			if (position2 == 100) {
+				System.out.println("player2 own the game");
+			}
+		}
+	}
+
+	static int position() {
+		count++;
+		return (int) (Math.random() * 10) % 6 + 1;
+
+	}
+
+	static int die() {
+		return (int) (Math.random() * 10) % 3;
+	}
+
+	public static void main(String[] args) {
+		SnakeLadder_Game obj1 = new SnakeLadder_Game();
+		obj1.checkWinner(obj1);
+	}
 }
